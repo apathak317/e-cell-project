8dcc707dd82784d5dfc4a544d3805833b896d552
apathak317 <apathak618188@gmail.com>
2026-03-23 20:47:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMessage() {
+  document.getElementById("output").innerText =
+    "Smart Waste Management System helps cities stay clean, efficient, and eco-friendly.";
+}
+
+function scrollToSection() {
+  document.getElementById("content").scrollIntoView({
+    behavior: "smooth"
+  });
+}
